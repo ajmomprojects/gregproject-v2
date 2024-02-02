0d462f714b7d811bffe2e4ded5c089be38907926
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2024-02-02 17:59:35 +0000

    migrate events table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  address    :string
+#  event_date :date
+#  lat        :float
+#  lng        :float
+#  notes      :string
+#  title      :string
+#  venue_name :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  city_id    :integer
+#  county_id  :integer
+#  state_id   :integer
+#  user_id    :integer
+#
 class Event < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_02_175724) do
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.date "event_date"
+    t.string "venue_name"
+    t.string "address"
+    t.string "notes"
+    t.float "lat"
+    t.float "lng"
+    t.integer "county_id"
+    t.integer "city_id"
+    t.integer "state_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
